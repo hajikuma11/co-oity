4955e44213f40bd9deb3b63f1055bb0807395abf
hajikuma11 <hajimekuma11@gmail.com>
2019-02-11 12:43:14 +0900

    Update index.php

--- index.php
+++ index.php
@@ -2,7 +2,27 @@
 echo 'phpのテストです。<br>';
 $dbinfo = parse_url(getenv('DATABASE_URL'));
 $dsn = 'pgsql:host=' . $dbinfo['host'] . ';dbname=' . substr($dbinfo['path'], 1);
-$pdo = new PDO($dsn, $dbinfo['user'], $dbinfo['pass']);
-var_dump($pdo->getAttribute(PDO::ATTR_SERVER_VERSION));
-phpinfo();
+
+try {
+ // データベースと接続
+ $dbh = new PDO($dsn, $dbinfo['user'], $dbinfo['pass']);
+ // テーブル作成のためのSQL
+ $sql = "CREATE TABLE menu (
+	id int PRIMARY KEY,
+	name varchar(50),
+	price int,
+	modify_datetime date,
+	create_datetime date
+)";
+ // SQLクエリ実行
+ $res = $dbh->query( $sql);
+ var_dump($res);
+
+} catch(PDOException $e) {
+
+ var_dump($e->getMessage());
+}
+
+// データベースの接続を切断
+$dbh = null;
 ?>
