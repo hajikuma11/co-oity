af066c4457b974db0cb805297d01884951258b7c
hajikuma11 <hajimekuma11@gmail.com>
2019-03-11 16:56:45 +0900

    CSSを追加、HTMLにテーブル追加

--- index.php
+++ index.php
@@ -1,33 +1,36 @@
 <!DOCTYPE html>
 <html lang="ja">
-<head>
+  <head>
     <meta charset="utf-8">
-    <title>title</title>
-    <meta name="description" content="どのような内容であるかを簡潔にまとめた概要">
+    <title>Oityマニュアル</title>
+    <meta name="description" content="Oityのマニュアルぺージ">
     <link rel="stylesheet" href="./style.css">
-</head>
-<body>
+  </head>
+  <body>
+    <header>
+      <h1>Oityマニュアル</h1>
+    </header>
+    
+    <table>
+        <tr>
+            <div class='midasi'>バス時刻表示機能</div>
+        </tr>
+        <tr>
+            <th>反応する文字列</th>
+            <th>概要</th>
+            <th>例</th>
+        </tr>
+        <tr>
+            <td>6bu</td>
+            <td>6時台の北山中央行きのバスの時刻を表示します。</td>
+            <td>User:6bu<br>Oity:6時12,34,56(この時刻は例えです)</td>
+        </tr>
+        
+    </table>
 
-<header>
-    <h1>見出しレベル1</h1>
-    <nav>
-        <ul>
-            <li><a href="./index.html">Home</a></li>
-        </ul>
-    </nav>
-</header>
+    <footer>
+      &copy; 2019/03/11
+    </footer>
 
-<article>
-    <h2>見出しレベル2</h2>
-    <p>コンテンツ内容(段落)</p>
-</article>
-
-<footer>
-    &copy; 2018 sitename.
-</footer>
-
-</body>
+  </body>
 </html>
-<?php
-
-?>
