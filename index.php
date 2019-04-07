15e328160bada9f7c5b1d1a9fc4a72842104c46a
hajikuma11 <40023230+hajikuma11@users.noreply.github.com>
2019-04-07 20:16:39 +0900

    Update index.php

--- index.php
+++ index.php
@@ -3,30 +3,59 @@
   <head>
     <meta charset="utf-8">
     <title>Oityマニュアル</title>
+    <link rel="stylesheet" href="style.css" type="text/css">
     <meta name="description" content="Oityのマニュアルぺージ">
-    <link rel="stylesheet" href="./style.css">
   </head>
   <body>
+
     <header>
       <h1>Oityマニュアル</h1>
+      <p1>Oityの使い方が乗っているヘルプページです。</p1>
     </header>
-    
+
+    <h2>オイティーのメニュー画面の機能</h2>
+
     <table>
-        <tr>
-            <th>バス時刻表 表示機能</th>
-        </tr>
-        <tr>
-            <th>反応する文字列</th>
-            <th>概要</th>
-            <th>例</th>
-        </tr>
-        <tr>
-            <td>6bu</td>
-            <td>6時台の北山中央行きのバスの時刻を表示します。</td>
-            <td>User:6bu<br>Oity:6時12,34,56<br>(この時刻は例えです)</td>
-        </tr>
-        
+          <tr>
+              <th>ボタン名</th>
+              <th>機能説明</th>
+          </tr>
+          <tr>
+              <td class="kotoba">週間天気予報</td>
+              <td class="kinou">大阪府、京都府、兵庫県、和歌山県の各地域ごとの<br>週間天気予報を表示します。</td>
+          </tr>
     </table>
+
+    <p1 class="danraku">バス時刻表 表示機能</p1>
+    <p2>
+      <table>
+            <tr>
+                <th>反応する言葉</th>
+                <th>機能説明</th>
+                <th>例</th>
+            </tr>
+            <tr>
+                <td class="kotoba">6bu,7bu,8bu...20bu,21bu</td>
+                <td class="kinou">指定した時間帯の<br><span>長尾駅発<br>北山中央行き</span><br>のバスの<span>時刻表</span>を表示します。</td>
+                <td class="rei"><span>User</span>:「6bu」<br><span>Oity</span>:「6時12,34,56」<br>(※ここの時刻表は例えです)</td>
+            </tr>
+            <tr>
+                <td class="kotoba">6bd,7bd,8bd...21bd,22bd</td>
+                <td class="kinou">指定した時間帯の<br><span>北山中央発<br>長尾駅行き</span><br>のバスの<span>時刻表</span>を表示します。</td>
+                <td class="rei"><span>User</span>:「6bd」<br><span>Oity</span>:「6時12,34,56」<br>(※ここの時刻表は例えです)</td>
+            </tr>
+            <tr>
+                <td class="kotoba">10kd,11kd,12kd...18kd,19kd</td>
+                <td class="kinou">指定した時間帯の<br><span>北山中央発<br>樟葉駅行き</span><br>のバスの<span>時刻表</span>を表示します。</td>
+                <td class="rei"><span>User</span>:「10kd」<br><span>Oity</span>:「10時12,34,56」<br>(※ここの時刻表は例えです)</td>
+            </tr>
+            <tr>
+                <td class="kotoba">10ku,11ku,12ku...20ku,21ku</td>
+                <td class="kinou">指定した時間帯の<br><span>樟葉駅発<br>北山中央行き</span><br>のバスの<span>時刻表</span>を表示します。</td>
+                <td class="rei"><span>User</span>:「10ku」<br><span>Oity</span>:「10時12,34,56」<br>(※ここの時刻表は例えです)</td>
+            </tr>
+      </table>
+    </p2>
 
     <footer>
       &copy; 2019/03/11
